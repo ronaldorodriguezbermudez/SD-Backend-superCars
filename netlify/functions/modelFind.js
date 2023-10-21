57a236bb4902b4c96422abd586918704715b4507
Ronaldo Rodríguez Bermúdez <ronaldo.rodriguez.bermudez@est.una.ac.cr>
2023-10-20 20:52:28 -0600

    Initial commit

---
+++ netlify/functions/modelFind.js
@@ -0,0 +1,25 @@
+"use strict"
+
+const mongoPromise = require('./mongoDB');
+
+const headers = require('./headersCORS');
+
+exports.handler = async (event, context) => {
+
+  if (event.httpMethod == "OPTIONS") {
+     return { statusCode: 200, headers, body: "OK" };
+  }
+	
+  try {
+    const client = await mongoPromise;
+    const id = parseInt(event.path.split("/").reverse()[0]);
+
+    const authors = 
+	  await client.db("supercars").collection("models").find({_id:id}).toArray();
+
+    return { statusCode: 200, headers, body: JSON.stringify(authors)};
+  } catch (error) {
+    console.log(error);
+    return { statusCode: 400, headers, body: JSON.stringify(error) };
+  }
+};
